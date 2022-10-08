d83198e517999b0e4683046b782660c31bf548fe
bethanyshoun <bethanyshoun@gmail.com>
2022-10-08 09:56:06 -0400

    updated mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -27,15 +27,12 @@ export const ADD_USER = gql`
 
 // SAVE_BOOK will execute the saveBook mutation.
 export const SAVE_BOOk = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
-    }
+  mutation saveBook($authors: String, $description: String, $title: String, $bookId: String!, $image: String, $link: String) {
   }
 `;
-
 // REMOVE_BOOK will execute the removeBook mutation.
+export const REMOVE_BOOk = gql`
+  mutation saveBook($_id: bookId) {
+
+  }
+`;
