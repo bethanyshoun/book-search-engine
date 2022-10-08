294b67ab72403ef03cd1805300bac036a597d09e
bethanyshoun <bethanyshoun@gmail.com>
2022-10-07 23:09:09 -0400

    updated files with comments and/or code to begin fleshing out requirements

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
 
+const { authMiddleware } = require(./schemas);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
