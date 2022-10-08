16142cef6a1eb0a9f6b89443f63688fa1e4a8468
bethanyshoun <bethanyshoun@gmail.com>
2022-10-08 08:41:41 -0400

    complete typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,44 +1,45 @@
-Query type:
+const { gql } = require('apollo-server-express');
 
-me: Which returns a User type.
-Mutation type:
+const typeDefs = gql`
+    type: User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type: Book {
+        bookId: String
+        authors: [Strings]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
 
-login: Accepts an email and password as parameters; returns an Auth type.
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+        thoughts(username: String): [Thought]
+        thought(_id: ID!): Thought
+    }
 
-addUser: Accepts a username, email, and password as parameters; returns an Auth type.
+    type Auth {
+        token: ID!
+        user: User
+    }
 
-saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(authors: [String], description: String, title: String, bookId: String!, image: String, link: String): User
+        removeBook(bookId: String!): User
+    }
+`;
 
-removeBook: Accepts a book's bookId as a parameter; returns a User type.
+module.exports = typeDefs;
 
-User type:
 
-_id
 
-username
 
-email
-
-bookCount
-
-savedBooks (This will be an array of the Book type.)
-
-Book type:
-
-bookId (Not the _id, but the book's id value returned from Google's Book API.)
-
-authors (An array of strings, as there may be more than one author.)
-
-description
-
-title
-
-image
-
-link
-
-Auth type:
-
-token
-
-user (References the User type.)
